7af62edcd7741b69cbbeff0807f975ae61cea8f3
meowrzboy <rommaosipov@gmail.com>
2025-09-21 16:32:33 +0300

    v6

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 let moveOrder = 1;
-let gameButton = document.querySelectorAll("button");
+let gameButton = document.querySelectorAll(".gameplay-box__grid-elem");
 let animation = document.querySelectorAll(".animation");
+let resultButton = document.querySelector(".result-block")
+// let gridElem1Counter = document.querySelector(".grid-elem1");
+// let gridElem2Counter = document.querySelector(".grid-elem2");
+// let gridElem3Counter = document.querySelector(".grid-elem3");
+// let gridElem4Counter = document.querySelector(".grid-elem4");
+// let gridElem5Counter = document.querySelector(".grid-elem5");
+// let gridElem6Counter = document.querySelector(".grid-elem6");
+// let gridElem7Counter = document.querySelector(".grid-elem7");
+// let gridElem8Counter = document.querySelector(".grid-elem8");
+// let gridElem9Counter = document.querySelector(".grid-elem9");
+
 
 gameButton.forEach(button => {
     button.onclick = function(){
@@ -11,18 +22,17 @@ gameButton.forEach(button => {
         }
         updateAnimationMoveOrder();
         updateMoveOrder(this);
-        console.log(moveOrder);
+        isDraw();
         if (this.classList.contains("gameplay-box__cross") || this.classList.contains("gameplay-box__zero")){
             gameButton.forEach(btn => {
-                    if (btn.classList.contains("gameplay-box__cross") || btn.classList.contains("gameplay-box__zero")) {
-                        let animationElement = btn.closest('.gameplay-box__grid-elem').querySelector('.animation');
-                        if (animationElement) {
-                            animationElement.classList.remove("cross_animation", "zero_animation");
-                        }
+                if (btn.classList.contains("gameplay-box__cross") || btn.classList.contains("gameplay-box__zero")) {
+                    let animationElement = btn.closest('.gameplay-box__grid-elem').querySelector('.animation');
+                    if (animationElement) {
+                        animationElement.classList.remove("cross_animation", "zero_animation");
                     }
+                }
             });
         };
-        return moveOrder;
     };
 });
 function isEven(num){
@@ -32,6 +42,22 @@ function isEven(num){
         return false;
     }
 };
+function isDraw() {
+    const allFilled = Array.from(gameButton).every(button => {
+        return button.classList.contains("gameplay-box__cross") || 
+        button.classList.contains("gameplay-box__zero");
+    });
+    if (allFilled) {
+        resultButton.textContent = "Draw !!!";
+        showRestartButton();
+    };  
+}
+function showRestartButton() {
+    const titleBlock = document.querySelector('.game-name-block');
+    const restartButton = document.querySelector('.restart-block');
+    titleBlock.classList.add('hidden');
+    restartButton.classList.add('visible');
+}
 function updateAnimationMoveOrder(){
     if (isEven(moveOrder) === false){
         animation.forEach(animation => {
